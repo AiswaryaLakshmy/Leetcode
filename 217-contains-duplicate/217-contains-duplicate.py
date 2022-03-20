9c185a92f464a8974b5e34f39a247b0b6a9331cc
Aiswarya <agopala2@binghamton.edu>
2022-03-19 22:53:12 -0400

    Time: 624 ms (45.09%), Space: 26 MB (32.12%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,6 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        #return len(nums) != len(set(nums))
         '''
         temp = []
         if not nums or len(nums) == 0:
@@ -14,5 +14,11 @@ class Solution:
             return False
         
         '''
-            
+        h = {}
+        for idx, val in enumerate(nums):
+            if val in h:
+                return True
+            else:
+                h[val] = idx
+        return False
         
